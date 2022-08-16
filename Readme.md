80d137f2da0a081aa332f8f1cc02cb8c7f1e79ec
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2022-08-16 14:12:17 +0530

    tables

---
+++ Readme.md
@@ -0,0 +1,16 @@
+html page
+style .css
+script.js
+
+//////
+css we are lot of code repeted code
+.red{
+    background-color:red;
+
+}
+.center{
+    text-align: center;
+}
+
+//js also need repeted code
+document.getElementById
---
+++ script.js
@@ -0,0 +1,9 @@
+function sum(x,y) {
+    return x+y
+}
+
+function sumBasedOnInput() {
+    let x = (Number) (document.getElementById("num1").value);
+    let y =  (Number)  (document.getElementById("num2").value);
+    return x+y //10
+}
